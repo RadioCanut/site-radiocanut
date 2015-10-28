804fb64ff3c9d52ffe06d20ba171f091b2db68cd
bubu <bubu@riseup.net>
2015-10-28 13:16:22 +0100

    On ajoute le gitignore et quelques fichiers de config

---
+++ config/mes_options.php
@@ -0,0 +1,10 @@
+<?php
+if (!isset($GLOBALS['z_blocs']))
+	$GLOBALS['z_blocs'] = array('content','aside','extra','head','head_js','header','footer','breadcrumb');
+	
+	define('_Z_AJAX_PARALLEL_LOAD','content,aside,extra');
+
+define('_ALBUMS_INSERT_HEAD_CSS',false);
+
+
+?>
