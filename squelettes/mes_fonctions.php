315993713ea02107a8664ddd39fdc28cb0c4eb35
t_cha <t-cha@users.noreply.github.com>
2016-10-08 10:19:53 +0200

    Base affichage et lecture des podcasts sur l'accueil

--- squelettes/mes_fonctions.php
+++ squelettes/mes_fonctions.php
@@ -2,4 +2,10 @@
 if (!isset($GLOBALS['z_blocs']))
 	$GLOBALS['z_blocs'] = array('content','aside','extra','head','head_js','header','footer','breadcrumb');
 
+function tags_to_sound_array($tags) {
+	$sons = array();
+	preg_match_all("/http[A-Za-z0-9_\.\-:\/]*?.mp3/", $tags, $sons);
+	return $sons[0];
+}
+
 ?>
