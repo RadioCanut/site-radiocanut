8291f6ea2d778d14a6ece3ea871a1c5a64b08d32
t_cha <t-cha@users.noreply.github.com>
2017-11-19 03:56:18 +0100

    Desactive z_ajax_parallel_load car actuellement la page est vide sans js

--- config/mes_options.php
+++ config/mes_options.php
@@ -2,7 +2,7 @@
 if (!isset($GLOBALS['z_blocs']))
 	$GLOBALS['z_blocs'] = array('content','aside','extra','head','head_js','header','footer','breadcrumb');
 	
-	define('_Z_AJAX_PARALLEL_LOAD','content,aside,extra');
+	//define('_Z_AJAX_PARALLEL_LOAD','content,aside,extra');
 
 define('_ALBUMS_INSERT_HEAD_CSS',false);
 
